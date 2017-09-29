2b28a755474dea0ec304b22aa03f47304a72ace8
Xiangzi <tumocpc@yahoo.com>
2017-09-29 09:11:37 -0700

    Update Regions.js

--- www/Regions.js
+++ www/Regions.js
@@ -17,10 +17,10 @@
  under the License.
  */
 
-var _ = require('com.unarin.cordova.beacon.underscorejs');
-var CircularRegion = require('com.unarin.cordova.beacon.CircularRegion');
-var BeaconRegion = require('com.unarin.cordova.beacon.BeaconRegion');
-var Region = require('com.unarin.cordova.beacon.Region');
+var _ = require('com.tumoc.cordova.beacon.underscorejs');
+var CircularRegion = require('com.tumoc.cordova.beacon.CircularRegion');
+var BeaconRegion = require('com.tumoc.cordova.beacon.BeaconRegion');
+var Region = require('com.tumoc.cordova.beacon.Region');
 
 
 /**
